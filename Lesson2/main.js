455914286e6290cdf7dd4e30b0d01c71400f7df2
itl-serg <itl.serg@gmail.com>
2021-07-27 19:10:05 +0300

    Lesson2 (1-3)

---
+++ Lesson2/main.js
@@ -0,0 +1,40 @@
+// Задание 1
+// alert("Задание #1");
+
+// let a = 2;
+// let b = a * 3;
+// let c = a + b;
+// console.log(a, b, c);
+
+// Задание 2
+// alert("Задание #2");
+
+// let firstName = prompt("Имя?", "John");
+// let lastName = prompt("Фамилия?", "Doe");
+// result = alert(`"What’s up ${firstName + " " + lastName}"`);
+// console.log(`"What’s up ${firstName + " " + lastName}"`);
+
+// //Задание 3
+// alert("Задание #3");
+
+// let x = prompt("Введите число X =");
+// let y = prompt("Введите число Y =");
+
+// let multiplication = x * y;
+// result = alert(`Произведение ${multiplication}`);
+// console.log(`x * y = ${multiplication}`);
+
+// let division = x / y;
+// result = alert(`Частное ${division}`);
+// console.log(`x / y = ${division}`);
+
+// let subtraction = x - y;
+// result = alert(`Разность  ${subtraction}`);
+// console.log(`x - y = ${subtraction}`);
+
+// let addition = Number(x) + Number(y);
+// result = alert(`Сумма  ${addition}`);
+// console.log(`x + y = ${addition}`);
+
+//Задание 4
+// alert("Задание #4");
