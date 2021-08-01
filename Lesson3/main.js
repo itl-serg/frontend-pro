7b8ba85b35f8046dbad7c34efafac87c3dfff7d2
itl-serg <itl.serg@gmail.com>
2021-08-01 23:54:27 +0300

    Lesson3 (1.1-1.9)

--- Lesson3/main.js
+++ Lesson3/main.js
@@ -42,3 +42,44 @@
 // 20 <= age && age < 27 ? alert("Выслать повестку") : false;
 
 //Задание 1.5
+
+// let minibus = prompt("Какой номер маршрутки приехал?");
+// minibus == 7 || minibus == 225 || minibus == 255 ? alert("Еду домой") : alert("Ожидаю");
+
+//Задание 1.6
+
+// let date = new Date();
+// let day = date.getDay();
+
+// let weekDay = new Array(7);
+// weekDay[0] = "Воскресенье";
+// weekDay[1] = "Понедельник";
+// weekDay[2] = "Вторник";
+// weekDay[3] = "Среда";
+// weekDay[4] = "Четверг";
+// weekDay[5] = "Пятница";
+// weekDay[6] = "Суббота";
+
+// day == 0 || day == 6
+//     ? alert(`Сегодня ${weekDay[date.getDay()]} выходной`)
+//     : alert(`Сегодня ${weekDay[date.getDay()]} идти на работу`);
+
+//Задание 1.7
+
+// let x = prompt("Введите значение 'x' от -20 до 20");
+// let y = prompt("Введите значение 'y' от -20 до 20");
+
+// (x <= 1 && y >= 3) || (x <= 1 && y < 0) ? alert(+x + +y) : alert("Неверно");
+
+//Задание 1.8
+
+// let x = prompt("Введите значение 'x'");
+// let y = prompt("Введите значение 'y'");
+
+// (x > 2 && x < 11) || (y >= 6 && y < 14) ? alert(`Переменная "x" = ${+x + 2}`) : alert(`Переменная "x" = ${+x + 5}`);
+
+//Задание 1.9
+
+let username = prompt("Введите свое имя");
+
+username ? alert(`Здравствуйте ${username}`) : alert("Вы не ввели имя");
