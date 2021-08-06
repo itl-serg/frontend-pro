5b604928fee7c98e56270483f96bac8147094f2c
itl-serg <itl.serg@gmail.com>
2021-08-06 17:07:28 +0300

    Lesson4(1-2)

--- Lesson4/main.js
+++ Lesson4/main.js
@@ -1,1 +1,27 @@
 // Задание 1
+
+// let a = [1, 2, 3, "a", "b", "c", "4", "5", "6"];
+// let sum = 0;
+// for (let i = 0; i < a.length; i++) {
+//     if (a[i] >= 1) {
+//         sum += +a[i];
+//     }
+// }
+// alert(`Сумма всех чисел : ${sum}`);
+
+// Задание 2
+
+// let a = ["AngularJS", "jQuery"];
+// a.unshift("Backbone.js");
+// a.push("ReactJS", "Vue.js");
+// a.splice(1, 0, "CommonJS");
+// console.log(a);
+// for (let i = 0; i < a.length; i++) {
+//     if (a[i] === "jQuery") {
+//         alert(`Это здесь лишнее "${a[i]}"`);
+//         a.splice([i], 1);
+//         console.log(a);
+//     }
+// }
+
+// Задание 3
