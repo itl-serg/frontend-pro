13dc62d684983dbc4c5e2a36319d5f42180c8c6d
itl-serg <itl.serg@gmail.com>
2021-08-08 22:56:10 +0300

    Lesson5 (1-2)

---
+++ Lesson5/main.js
@@ -0,0 +1,23 @@
+// Задание 1
+
+// function max(a, b) {
+//     if (a < b) return b;
+//     else return a;
+// }
+// console.log(max(0, -1));
+
+// Задание 2
+
+// function min() {
+//     let number = [];
+
+//     for (let i = 0; i < arguments.length; i++) {
+//         number[i] = arguments[i];
+//     }
+//     let minNum = Math.min.apply(null, number);
+//     return minNum;
+// }
+
+// console.log(min(0, -1, 100, 500, 100500));
+
+// Задание 3
